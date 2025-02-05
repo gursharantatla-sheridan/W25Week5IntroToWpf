ed5e81ade197bd3aabab3365eed30d9f51586c28
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-02-05 10:51:34 -0500

    msg box

--- Week5IntroToWpf/MainWindow.xaml.cs
+++ Week5IntroToWpf/MainWindow.xaml.cs
@@ -20,5 +20,15 @@ namespace Week5IntroToWpf
         {
             InitializeComponent();
         }
+
+        private void btnClickMe_Click(object sender, RoutedEventArgs e)
+        {
+            var result = MessageBox.Show("Hello world", "Message", MessageBoxButton.YesNo, MessageBoxImage.Information);
+
+            if (result == MessageBoxResult.Yes)
+                MessageBox.Show("YES button clicked");
+            else
+                MessageBox.Show("NO button clicked");
+        }
     }
 }
